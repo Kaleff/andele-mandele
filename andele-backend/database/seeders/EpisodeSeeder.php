8fa1010cd1295b7c914c6da8d8d85c7197dc217d
Marks Kalijevs <markskalijevs@gmail.com>
2024-06-07 08:37:35 +0300

    Create seeder for Episodes

--- andele-backend/database/seeders/EpisodeSeeder.php
+++ andele-backend/database/seeders/EpisodeSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Http\Controllers\EpisodeController;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,9 @@ class EpisodeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $this->command->question("Starting to seed Episode table");
+        $controller = new EpisodeController();
+        $result = $controller->store();
+        $this->command->info($result);
     }
 }
