1bc2a6e41a3b923fa723361d2ca73bc5ee37f2da
Marks Kalijevs <markskalijevs@gmail.com>
2024-06-07 07:51:39 +0300

    Make trait scroll through the api pages

--- andele-backend/app/Traits/CallApiTrait.php
+++ andele-backend/app/Traits/CallApiTrait.php
@@ -3,22 +3,39 @@
 namespace App\Traits;
 
 trait CallApiTrait {
-    public function callApi(String $parameter) 
+    public function callApi(String $parameter)
     {
-        $curl = curl_init();
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => "https://rickandmortyapi.com/api/" . $parameter,
-            CURLOPT_HTTPHEADER => array("Content-Type: text/plain"),
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "GET"
-        ));
-        $response = curl_exec($curl);
-        curl_close($curl);
+        $reached_the_end = false;
+        $page = 1;
+        $api_url = "https://rickandmortyapi.com/api/$parameter?page=$page";
+        $results = [];
+        while(!$reached_the_end) {
+            $curl = curl_init();
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => $api_url,
+                CURLOPT_HTTPHEADER => array("Content-Type: text/plain"),
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => "",
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => "GET"
+            ));
+            $response = curl_exec($curl);
+            curl_close($curl);
+            $data = json_decode($response, true);
+            $results = array_merge($results, $data['results']);
+            if($data['info']['next'] == null) {
+                $reached_the_end = true;
+            } else {
+                $page++;
+                $api_url = "https://rickandmortyapi.com/api/$parameter?page=$page";
+            }
+        }
+        dd($results);
+        
+        // Convert the data to json and back from json to get assoc. array instead of object for provided data
         return $response;
     }
 }
