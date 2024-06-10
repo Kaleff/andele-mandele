4f265988a0e1e2efbca192a9c4ba86ecb3557ddc
Marks Kalijevs <markskalijevs@gmail.com>
2024-06-10 23:44:25 +0300

    Comment out Request

--- andele-backend/routes/api.php
+++ andele-backend/routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\EpisodeController;
-use Illuminate\Http\Request;
+// use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*Route::get('/user', function (Request $request) {
